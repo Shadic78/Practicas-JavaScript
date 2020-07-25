23d475d108af7abb78ca118f427d96a3d4ef70e5
Shadic78 <carlosjosue7889@gmail.com>
2020-07-25 10:54:08 -0500

    Proyecto localStorage

---
+++ ProyectoLocalStorage/js/App.js
@@ -0,0 +1,67 @@
+let listaArticulos = [];
+
+const crearArticulo = (titulo, contenido) => {
+    return {
+        titulo,
+        contenido
+    }
+}
+
+const agregarArticulo  = ( {titulo, contenido} ) => {
+    const contenedor = document.getElementById('contenidoLista');
+    const nuevoArticulo = document.createElement('div');
+    nuevoArticulo.innerHTML = `
+        <div class="card mb-4">
+            <div class="card-body">
+                <strong>Titulo: </strong>${titulo}
+                <strong>Contenido:</strong>${contenido}
+            </div>
+        </div>
+    `;
+    contenedor.appendChild(nuevoArticulo);
+}
+
+const cargarArticulos = () => {
+    listaArticulos = JSON.parse(window.localStorage.getItem('listaArticulos'));
+    console.log('lista recuperada: ');
+    console.log(listaArticulos);
+    listaArticulos.forEach(articulo => {
+        agregarArticulo(articulo);
+    });
+}
+
+const guardarEnLocalStorage = (lista) => {
+    localStorage.setItem('listaArticulos', JSON.stringify(lista));
+}
+
+const hayInformacionGuardada = () => {
+    if(localStorage.getItem('listaArticulos')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+window.onload = () => {
+    console.log(hayInformacionGuardada());
+    if(hayInformacionGuardada()) {
+        cargarArticulos();
+    }
+    else {
+        console.log('No hay archivos en localStorage');
+    }
+}
+
+document.getElementById('formulario').addEventListener('submit', (e) => {
+    const titulo = document.getElementById('txtTitulo').value;
+    const contenido = document.getElementById('txtContenido').value;
+    const articulo = crearArticulo(titulo, contenido);
+
+    agregarArticulo(articulo);
+    listaArticulos.push(articulo);
+
+    guardarEnLocalStorage(listaArticulos);
+
+    e.preventDefault();
+});
